80f3f08b1b08900c353d16d42b466d0a7be6a327
Palade Arina <paladearina@yahoo.com>
2026-09-15 10:13:06 +0300

    Add files via upload

---
+++ Functia PRINT 15-09-2026.py
@@ -0,0 +1,8 @@
+a=2
+b=9
+print('suma' , a,'+',b, '=', a+b)
+print('produsul' , a,'*',b, '=', a*b)
+print('impartirea' , b,'/',a, '=', b/a)
+print('cat' , b,'//',a, '=', b//a)
+print('rest' , b,'%',a, '=', b%a)
+print('ridicare la putere' , b,'^',a, '=', b^a)
