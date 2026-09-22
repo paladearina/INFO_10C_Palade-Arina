fc882b04ba764d6fade3d081c0d36115c36f1827
Palade Arina <paladearina@yahoo.com>
2026-09-22 10:08:26 +0300

    Add files via upload

---
+++ 22 sep 10c.py
@@ -0,0 +1,6 @@
+lungime=float(input('Lungime='))
+latime=float(input('Latime='))
+aria=lungime*latime
+perimetru=2*(lungime+latime)
+print('Aria dreptunghiului =' ,round(aria,2))
+print('Perimetrul dreptunghiului =' ,round(perimetru,4))
